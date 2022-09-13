be3a438cb022058d09d101a6b2d00f2465876c84
SAHIL0455 <sahilpawar1004@gmail.com>
2022-09-13 19:29:50 +0530

    ADDED: check string is anagram or not

--- src/com/bridgelabz/AnagramDetection.java
+++ src/com/bridgelabz/AnagramDetection.java
@@ -1,7 +1,20 @@
 package com.bridgelabz;
 
+import java.util.Arrays;
+
 public class AnagramDetection {
     public static void main(String[] args) {
-        System.out.println();
+        String word1 = "heart";
+        String word2 = "earthf";
+        char arr1[] = word1.toCharArray();
+        char arr2[] = word2.toCharArray();
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+        boolean result = Arrays.equals(arr1, arr2);
+        if (result == true) {
+            System.out.println("This is the Anagram");
+        } else {
+            System.out.println("This is Not Anagram");
+        }
     }
 }
